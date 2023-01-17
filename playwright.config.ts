1977e73f8eba429b1bdb2553362e17ed26d9bbfd
Diego <cortesroadiegoalejandro@gmail.com>
2023-01-16 20:55:28 -0500

    develop: added headless true in CI execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://www.saucedemo.com",
-    headless: false,
+    headless: process.env.CI ? true : false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on",
   },
