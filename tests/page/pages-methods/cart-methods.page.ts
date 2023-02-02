96bc3b3cc4d83793765d924c7cc4112966111a6c
Diego Cortés <60171460+Diegocortes15@users.noreply.github.com>
2023-02-02 07:28:12 -0500

    Develop (#20)
    * TCPW_0014: Added test case PW-0014 (#2)
    
    * TCPW_0014: Added test case PW-0014
    
    * TCPW_0014: Fitting test case summary and renamed method in product-methods.page
    
    * develop: added headless true in CI execution
    
    * Tcpw 0016 0017 (#4)
    
    * Draft
    
    * TCPW_0016_0017: Added test cases pw-0016 and pw-0017
    
    * hotfix: Fixing merge conflics main from develop (#6)
    
    * hotfix: Fitting POO of prodcuts-method.page.ts (#7)
    
    * PW-0019: Added test case pw-0019 (#9)
    
    * Develop: Update README.md
    
    * pw-0001: Improving assertions test cases pw-0006-0007-0008-0009 (#12)
    
    * TCPW_0021_0021_0022: Test cases Added (#14)
    
    * Fitting playwright.yml and test cases PW-0021-0022
    
    * Accessibility testing (#16)
    
    * accessibility-testing: Added accessibility test case
    
    * accessibility-testing: Fitting README.md
    
    * accessibility-testing: Fitting README.md
    
    * Hotfix (#17)
    
    * Develop (#15)
    
    * TCPW_0014: Added test case PW-0014 (#2)
    
    * TCPW_0014: Added test case PW-0014
    
    * TCPW_0014: Fitting test case summary and renamed method in product-methods.page
    
    * develop: added headless true in CI execution
    
    * Tcpw 0016 0017 (#4)
    
    * Draft
    
    * TCPW_0016_0017: Added test cases pw-0016 and pw-0017
    
    * hotfix: Fixing merge conflics main from develop (#6)
    
    * hotfix: Fitting POO of prodcuts-method.page.ts (#7)
    
    * PW-0019: Added test case pw-0019 (#9)
    
    * Develop: Update README.md
    
    * pw-0001: Improving assertions test cases pw-0006-0007-0008-0009 (#12)
    
    * TCPW_0021_0021_0022: Test cases Added (#14)
    
    * Fitting playwright.yml and test cases PW-0021-0022
    
    * Accessibility testing (#16)
    
    * accessibility-testing: Added accessibility test case
    
    * accessibility-testing: Fitting README.md
    
    * accessibility-testing: Fitting README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * hotfix: Fitting TCPW-0019-0023
    
    * hotfix: Fitting support-factory (#19)

--- tests/page/pages-methods/cart-methods.page.ts
+++ tests/page/pages-methods/cart-methods.page.ts
@@ -1,12 +1,14 @@
 import {Page, TestInfo} from "@playwright/test";
 import {PlaywrightFactory} from "../../utils/playwright-factory.utils";
+import {SupportFactory} from "../../utils/support-factory.utils";
 
 export class CartPageMethods {
   private readonly _page: Page;
   private readonly _testInfo: TestInfo;
   private readonly _playwrightFactory: PlaywrightFactory;
+  private readonly _supportFactory: SupportFactory;
   private readonly _pageName: string;
-  public cartItemsAdded: {itemName: string | null; price: string | null}[];
+  public cartItemsAdded: string;
 
   /**
    * @param {import('@playwright/test').Page} page
@@ -17,28 +19,32 @@ export class CartPageMethods {
     this._page = page;
     this._testInfo = testInfo;
     this._playwrightFactory = new PlaywrightFactory(this._page, this._testInfo);
+    this._supportFactory = new SupportFactory(this._page, this._testInfo);
     this._pageName = "cart-locators.page";
-    this.cartItemsAdded = [];
+    this.cartItemsAdded;
   }
 
   public async clickCheckoutButton(): Promise<void> {
     await this._playwrightFactory.click(this._pageName, "buttonCheckout");
   }
 
-  public async getCartProducts(): Promise<void> {
+  public async getCartProducts(): Promise<string> {
+    const products: {itemName: string | null; price: string | null}[] = [];
     const productListLenght = await (
       await this._playwrightFactory.getElementSelector(this._pageName, "itemCartList")
     ).count();
     for (let index = 0; index < productListLenght; index++) {
-      this.cartItemsAdded.push({
+      products.push({
         itemName: await this._playwrightFactory.getTextByIndex(this._pageName, "itemName", index),
         price: await this._playwrightFactory.getTextByIndex(this._pageName, "itemPrice", index),
       });
     }
+    this.cartItemsAdded = await this._supportFactory.jsonToString(products);
+    return this.cartItemsAdded;
   }
 
-  async verifyProductsAdded(expectedProductsAdded: object) {
-    await this.getCartProducts();
-    await this._playwrightFactory.verifyCompareValues(expectedProductsAdded, this.cartItemsAdded);
+  async verifyProductsAdded(expectedProductsAdded: string) {
+    const products = await this.getCartProducts();
+    await this._playwrightFactory.verifyCompareValues(products, expectedProductsAdded);
   }
 }
